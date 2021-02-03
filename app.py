c5b40236d0c525381733bd391aef7950a3ada694
Ahmad <minwer.eid.ahmad@gmail.com>
2021-02-03 11:59:19 +0200

    add delete and update endpointes

--- app.py
+++ app.py
@@ -17,9 +17,9 @@ migrate = Migrate(app, db)
 
 class Todo(db.Model):
     __tablename__ = 'todos'
-    id = db.Column( db.Integer, primary_key=True )
-    description = db.Column( db.String(), nullable=False )
-    completed = db.Column( db.Boolean, nullable=False, default=False)
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
 
     def __repr__ (self):
         return f'<Todo {self.id} {self.description}>'
@@ -27,7 +27,7 @@ class Todo(db.Model):
 
 @app.route('/')
 def index():
-    return render_template( "index.html", data=Todo.query.all() )
+    return render_template( "index.html", data=Todo.query.order_by('id').all() )
 
 
 @app.route('/todos/create', methods=['POST'])
@@ -50,6 +50,45 @@ def create_todo():
         abort(400)
     else:
         return jsonify(body)
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+    error = False
+    try:
+        completed = request.get_json()['completed']
+        todo = Todo.query.get(todo_id)
+        todo.completed = completed
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return redirect(url_for('index'))
+        
+@app.route('/todos/<todo_id>', methods=['DELETE'])
+def delete_todo(todo_id):
+    error = False
+    try:
+        todo = Todo.query.get(todo_id)
+        db.session.delete(todo)
+        db.session.commit()
+    except:
+        error = True
+        db.session.rollback()
+        print(sys.exc_info())
+    finally:
+        db.session.close()
+    if error:
+        abort(400)
+    else:
+        return jsonify({ 'success': True })
+
+
 
 if __name__ == '__main__':
     app.run()
